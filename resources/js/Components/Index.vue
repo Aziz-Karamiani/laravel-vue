c10a29df731879902b3b75da041f5fee4df4eb71
a.karamiani <aziz.karamiani@gmail.com>
2022-11-11 09:57:37 +0330

    Vue router-link

--- resources/js/Components/Index.vue
+++ resources/js/Components/Index.vue
@@ -1,5 +1,11 @@
 <template>
-    <router-view></router-view>
+    <div>
+        <div>
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
+        </div>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
