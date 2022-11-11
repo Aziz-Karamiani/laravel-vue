264443a6cfd950923964e355c1a1d13f68af828c
a.karamiani <aziz.karamiani@gmail.com>
2022-11-11 15:09:24 +0330

    Using props

--- resources/js/Components/Bookables/BookableListItem.vue
+++ resources/js/Components/Bookables/BookableListItem.vue
@@ -1,15 +1,20 @@
 <template>
-    <div class="card" style="width: 18rem;">
+    <div class="card m-2">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">{{ title }}</h5>
+            <p class="card-text">{{ content }}</p>
+            <a href="#" class="btn btn-primary">Book: {{ price }}</a>
         </div>
     </div>
 </template>
 
 <script>
 export default {
+    props: {
+        title: String,
+        content: String,
+        price: Number
+    }
 }
 </script>
 
--- resources/js/Components/Bookables/Bookables.vue
+++ resources/js/Components/Bookables/Bookables.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="container">
-        <bookable-list-item></bookable-list-item>
-        <bookable-list-item></bookable-list-item>
-        <bookable-list-item></bookable-list-item>
-        <bookable-list-item></bookable-list-item>
+        <bookable-list-item title="First Villa" content="Some quick example text to build on the card title and make up the bulk of the card's content." :price="1000"></bookable-list-item>
+        <bookable-list-item title="First Villa2" content="Some quick example text to build on the card title and make up the bulk of the card's content2." :price="2000"></bookable-list-item>
+        <bookable-list-item title="First Villa3" content="Some quick example text to build on the card title and make up the bulk of the card's content3." :price="3000"></bookable-list-item>
     </div>
 </template>
 
