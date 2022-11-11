a3efe3f9c7c8622e38bf2dee4a909c9b874b61c0
a.karamiani <aziz.karamiani@gmail.com>
2022-11-11 15:46:08 +0330

    Vue data flow

--- resources/js/Components/Bookables/Bookables.vue
+++ resources/js/Components/Bookables/Bookables.vue
@@ -1,16 +1,41 @@
 <template>
     <div class="container">
-        <bookable-list-item title="First Villa" content="Some quick example text to build on the card title and make up the bulk of the card's content." :price="1000"></bookable-list-item>
-        <bookable-list-item title="First Villa2" content="Some quick example text to build on the card title and make up the bulk of the card's content2." :price="2000"></bookable-list-item>
-        <bookable-list-item title="First Villa3" content="Some quick example text to build on the card title and make up the bulk of the card's content3." :price="3000"></bookable-list-item>
+        <bookable-list-item :title="this.bookable1.title"
+                            :content="this.bookable1.content"
+                            :price="1000"></bookable-list-item>
+        <bookable-list-item :title="this.bookable2.title"
+                            :content="this.bookable2.content"
+                            :price="2000"></bookable-list-item>
     </div>
 </template>
 
 <script>
 import BookableListItem from "./BookableListItem.vue";
+
 export default {
     components: {
         BookableListItem
+    },
+    data() {
+        return {
+            bookable1: {
+                "title": "Very cheep villa.",
+                "content": "This is a Very cheep villa.",
+                "price": 15000
+            },
+            bookable2: {
+                "title": "Very cheep villa1.",
+                "content": "This is a Very cheep villa1.",
+                "price": 15000
+            },
+        }
+    },
+
+    mounted() {
+        setTimeout(() => {
+            this.bookable1.title = "Very expensive villa";
+            this.bookable2.title = "Very expensive villa1";
+        }, 5000)
     }
 }
 </script>
