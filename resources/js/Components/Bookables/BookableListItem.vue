4be7bf05c6e22c78491e4a1bb27a1e6a8d0a6186
a.karamiani <aziz.karamiani@gmail.com>
2022-11-12 16:04:39 +0330

    Bookable link

--- resources/js/Components/Bookables/BookableListItem.vue
+++ resources/js/Components/Bookables/BookableListItem.vue
@@ -2,6 +2,7 @@
     <div>
         <div class="card m-1">
             <div class="card-body">
+                <router-link class="link" :to="{name: 'bookable', params: {bookable: id}}">Bookables</router-link>
                 <h5 class="card-title">{{ title }}</h5>
                 <p class="card-text">{{ content }}</p>
                 <a href="#" class="btn btn-primary">Book: {{ id }}</a>
