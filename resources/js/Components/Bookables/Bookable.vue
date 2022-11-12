6f4428dea48cab59d535faa0b92d9a66c07e9543
a.karamiani <aziz.karamiani@gmail.com>
2022-11-12 22:02:21 +0330

    Minor changes

--- resources/js/Components/Bookables/Bookable.vue
+++ resources/js/Components/Bookables/Bookable.vue
@@ -1,7 +1,18 @@
 <template>
-    <div class="container" v-if="bookable !== null">
-        <h2>{{ bookable.title }}</h2>
-        <p>{{ bookable.content }}</p>
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <div v-if="bookable !== null">
+                        <h2 class="card-title">{{ bookable.title }}</h2>
+                        <p class="card-text">{{ bookable.content }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            Availability
+        </div>
     </div>
 </template>
 
