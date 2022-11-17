e4601138a99149fe2328e7b2126fc5437594e5dd
a.karamiani <aziz.karamiani@gmail.com>
2022-11-17 14:36:41 +0330

    Check for review existing

--- resources/js/Components/Review/Review.vue
+++ resources/js/Components/Review/Review.vue
@@ -1,15 +1,20 @@
 <template>
     <div class="container">
-        <div class="form-group my-3">
-            <label for="rating">Make a vote (1 is worth and 5 is the best.)</label>
-            <star-rating v-model="review.rating" id="rating" class="fa-3x"></star-rating>
+        <div v-if="alreadyReviewed">
+            <h3 class="p-3">You already have a review!.</h3>
         </div>
-        <div class="form-group">
-            <label for="content">Content Review: </label>
-            <textarea name="content" id="content" cols="30" rows="10" class="form-control"
-                      placeholder="Content...." v-model="review.content"></textarea>
+        <div v-else>
+            <div class="form-group my-3">
+                <label for="rating">Make a vote (1 is worth and 5 is the best.)</label>
+                <star-rating v-model="review.rating" id="rating" class="fa-3x"></star-rating>
+            </div>
+            <div class="form-group">
+                <label for="content">Content Review: </label>
+                <textarea name="content" id="content" cols="30" rows="10" class="form-control"
+                          placeholder="Content...." v-model="review.content"></textarea>
+            </div>
+            <button class="btn btn-primary w-100 mt-3">Submit</button>
         </div>
-        <button class="btn btn-primary w-100 mt-3">Submit</button>
     </div>
 </template>
 
@@ -26,7 +31,19 @@ export default {
             review: {
                 rating: 5,
                 content: null
-            }
+            },
+            existingReview: null
+        }
+    },
+    created() {
+        axios.get(`/api/reviews/${this.$route.params.review}`)
+            .then(response => this.existingReview = response.data.data)
+            .catch(error => {
+            });
+    },
+    computed: {
+        alreadyReviewed() {
+            return this.existingReview !== null;
         }
     }
 }
