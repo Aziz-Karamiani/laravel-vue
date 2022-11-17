0bfec74971db51bdacf90ddaa8c0286bf110550a
a.karamiani <aziz.karamiani@gmail.com>
2022-11-17 18:24:10 +0330

    Booking controller minor changes

--- app/Http/Controllers/API/BookingController.php
+++ app/Http/Controllers/API/BookingController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Booking;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
     public function show(Request $request, $review)
     {
-
+        return Booking::getByReviewId($review) ?? abort(404);
     }
 }
