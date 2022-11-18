ab72cf35ff785b95da5fbeeca4477e5c5f0ae486
a.karamiani <aziz.karamiani@gmail.com>
2022-11-18 17:59:19 +0330

    Some bug fixed

--- resources/js/Components/Bookables/ReviewList.vue
+++ resources/js/Components/Bookables/ReviewList.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="my-3 d-none d-md-block border-start border-3 border-gray-700 p-3">
         <h5 class="text-secondary font-weight-bolder">Review List...</h5>
-        <div class="border-bottom border-2 pt-2" v-for="(review, index) in reviews" :key="index">
-            <div class="row">
-                <div class="col-md-6 fw-bold text-dark">Review Title</div>
-                <div class="col-md-6 justify-content-end">
-                    <star-rating :value="review.rating"></star-rating>
+        <div v-if="reviews !== null">
+            <div class="border-bottom border-2 pt-2" v-for="(review, index) in reviews" :key="index">
+                <div class="row">
+                    <div class="col-md-6 fw-bold text-dark">Review Title</div>
+                    <div class="col-md-6 justify-content-end">
+                        <star-rating v-model="review.rating"></star-rating>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <span>{{ review.created_at }}</span>
-            </div>
-            <div class="row py-2 text-secondary">
-                <p>{{ review.content }}</p>
+                <div class="row">
+                    <span>{{ review.created_at }}</span>
+                </div>
+                <div class="row py-2 text-secondary">
+                    <p>{{ review.content }}</p>
+                </div>
             </div>
         </div>
     </div>
